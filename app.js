f1161aba737a14311cbfab6b6005bd77b94b7534
Leoatomic <69369111+Leoatomic@users.noreply.github.com>
2021-08-31 11:50:09 +0200

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ particlesJS('particles-js',
         "speed": 3
       },
       "repulse": {
-        "distance": 200,
+        "distance": 600,
         "duration": 0.4
       },
       "push": {
